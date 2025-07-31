b2d20973df54975bdf125a57a1ae469886652b34
Heet Bhuva <heetbhuva1405@gmail.com>
2025-08-01 03:47:25 +0530

    Socket cors set

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,12 +1,10 @@
 import { io } from "socket.io-client";
 //https://algo-byte.onrender.com
 //http://localhost:3000
-const socket = io(
-  "https://algobyte.live",
+const socket = io({
+  withCredentials: true,
+  transports: ["websocket", "polling"],
+});
 
-  {
-    withCredentials: true,
-  }
-);
 
 export default socket;
