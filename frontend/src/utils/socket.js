239e06c2d08c82aeaef7e6317a32e59ae301485d
Heet Bhuva <heetbhuva1405@gmail.com>
2025-08-01 03:41:05 +0530

    Socket changes

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 //https://algo-byte.onrender.com
 //http://localhost:3000
 const socket = io(
-  "http://localhost:3000",
+  "https://algobyte.live",
 
   {
     withCredentials: true,
